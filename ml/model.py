f07eba6c4b1699d2d2eb180a40b96c0b3fe3e5bf
前田 <qiantiansun9@gmail.com>
2022-09-28 17:25:54 +0900

    データオーグメンテーションの削除

--- ml/model.py
+++ ml/model.py
@@ -1,12 +1,10 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
-from data_augmentation import data_augmentation
 
 
 def neural_voice_judgment_model():
     model = Sequential()
 
-    model.add(data_augmentation())
     model.add(Conv2D(8, (5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(16, (5, 5), activation='relu'))
