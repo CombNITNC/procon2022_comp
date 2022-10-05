e836f7d1bea058fae24cf454df7886918966d308
MikuroXina <ryosukadnak@gmail.com>
2022-10-05 21:57:51 +0900

    Refactor with find_answers

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from solver.card import CardIndex, \
     should_pick_cards_from_yaml
 from solver.const import ScoreConstant
 from solver.pick_ways import solve_by_binary_search
-from solver.request.meta import Answer
+from solver.request.meta import AbstractRequester, Answer, Match, Problem
 from ml.maesyori import preprocess_input
 from os import getenv
 from os.path import join, exists
@@ -69,59 +69,75 @@ def main():
 
     while True:
         problem = req.get_problem()
-        current_state.current_problem_id = problem.id
-        score_max = 0
-        answers: list[Answer] = []
-
-        for using_chunks in range(1, problem.chunks + 1):
-            chunks = req.get_chunks(using_chunks, TEMP_YAML_DIR)
-
-            images = []
-            for chunk in chunks:
-                image = preprocess_input(chunk.wav)
-                images.append(image)
-            prediction: ndarray = model.predict(np.array(images))
-            prediction_avg = np.average(prediction, axis=0)
-
-            probabilities: list[float] = prediction_avg.tolist()
-            print("probabilities: ", probabilities)
-            for index in range(0, len(probabilities)):
-                should.insert(
-                    current_state.current_problem_id,
-                    CardIndex(index + 1), probabilities[index]
-                )
-            should.set_picks_on(current_state.current_problem_id, problem.data)
-
-            solution = solve_by_binary_search(match.problems, should)
-            if solution is not None:
-                picks_by_problem, acc = solution
-                corrects = ceil(problem.data * acc)
-                fails = ceil(problem.data * (1 - acc))
-                score = calc_score(
-                    corrects,
-                    fails,
-                    using_chunks,
-                    const=score_const
-                )
-                if score_max < score:
-                    score_max = score
-                    for picks in picks_by_problem:
-                        answers.append(Answer(
-                            problem_id=problem.id,
-                            answers=list(map(
-                                lambda card_index: card_index.as_0_pad(),
-                                picks,
-                            ))
-                        ))
-
-                should.save_yaml(PICK_CARDS_YAML)
-                current_state.save_yaml(STATE_YAML)
-                break
-
-            print(f'solution not found with using {using_chunks} chunks')
+        answers = find_answers(
+            model, req, match, score_const, should, current_state, problem,
+        )
 
         for answer in answers:
+            print(answer)
             req.post_answer(answer)
+
+
+def find_answers(
+    model: tf.keras.Model,
+    req: AbstractRequester,
+    match: Match,
+    score_const: ScoreConstant,
+    should: ShouldPickCardsByProblem,
+    current_state: SolverState,
+    problem: Problem,
+) -> list[Answer]:
+    current_state.current_problem_id = problem.id
+    score_max = 0
+    answers: list[Answer] = []
+
+    for using_chunks in range(1, problem.chunks + 1):
+        chunks = req.get_chunks(using_chunks, TEMP_YAML_DIR)
+
+        images = []
+        for chunk in chunks:
+            image = preprocess_input(chunk.wav)
+            images.append(image)
+        prediction: ndarray = model.predict(np.array(images))
+        prediction_avg = np.average(prediction, axis=0)
+
+        probabilities: list[float] = prediction_avg.tolist()
+        for index in range(0, len(probabilities)):
+            should.insert(
+                current_state.current_problem_id,
+                CardIndex(index + 1),
+                probabilities[index],
+            )
+        should.set_picks_on(current_state.current_problem_id, problem.data)
+
+        solution = solve_by_binary_search(match.problems, should)
+        if solution is not None:
+            picks_by_problem, acc = solution
+            corrects = ceil(problem.data * acc)
+            fails = problem.data - corrects
+            score = calc_score(
+                corrects,
+                fails,
+                using_chunks,
+                const=score_const
+            )
+            if score_max < score:
+                score_max = score
+                for picks in picks_by_problem:
+                    answers.append(Answer(
+                        problem_id=problem.id,
+                        answers=list(map(
+                            lambda card_index: card_index.as_0_pad(),
+                            picks,
+                        )),
+                    ))
+
+            should.save_yaml(PICK_CARDS_YAML)
+            current_state.save_yaml(STATE_YAML)
+            return answers
+
+        print(f'solution not found with using {using_chunks} chunks')
+    return []
 
 
 if __name__ == '__main__':
