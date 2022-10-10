f3f6c7076bff85ee2a920f41173d049aad1ab392
前田 <qiantiansun9@gmail.com>
2022-10-10 20:44:32 +0900

    波形全体のメルスペクトル画像を生成するように修正

--- ml/maesyori.py
+++ ml/maesyori.py
@@ -1,5 +1,4 @@
-import random
-
+import cv2
 import numpy as np
 import scipy
 from librosa import amplitude_to_db, feature, note_to_hz
@@ -8,21 +7,19 @@ sr = 48000
 
 
 def preprocess_input(onsei_data: np.ndarray) -> np.ndarray:
-    slice_len = int(sr * 1.27)
-    kiritori = random.randrange(0, max(1, len(onsei_data) - slice_len))
-    sliced = np.resize(onsei_data[kiritori:kiritori+slice_len], slice_len)
 
     # プリエンファシスフィルタ:
     # y(t) = x(t) - p x(t - 1)
     p = 0.97
-    pre_emphasis = scipy.signal.lfilter([1.0, -p], 1, sliced)
+    pre_emphasis = scipy.signal.lfilter([1.0, -p], 1, onsei_data)
 
     hop_length = int(sr * 10e-3)
     win_length = int(sr / note_to_hz('C3'))
     mel_spectrogram = feature.melspectrogram(
         sr=sr, y=pre_emphasis, hop_length=hop_length, win_length=win_length)
-    mel_non_zero = np.where(mel_spectrogram == 0,
-                            np.finfo(float).eps, mel_spectrogram)
+    shrink = cv2.resize(mel_spectrogram, (128, 128))
+    mel_non_zero = np.where(shrink == 0,
+                            np.finfo(float).eps, shrink)
     common_logarithm = amplitude_to_db(mel_non_zero)
     max_value = common_logarithm.max()
     min_value = common_logarithm.min()
